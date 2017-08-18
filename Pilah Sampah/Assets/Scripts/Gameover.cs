5ed0c85997eb1c0cb3723acc88f39a3f9d5497b9
yusronhanan <yusronzain@gmail.com>
2017-08-18 09:42:04 +0700

    done

---
+++ Pilah Sampah/Assets/Scripts/Gameover.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Gameover : MonoBehaviour {
+
+	float timer = 0;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		timer += Time.deltaTime;
+		if(timer > 2)
+		{
+			Data.score = 0;
+			SceneManager.LoadScene ("Gameplay", LoadSceneMode.Single);
+		}
+	}
+}
